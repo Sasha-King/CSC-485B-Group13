45ff4fdb41db4ed89f05ad3c63052039e8b584cf
Kantaş Onur Kılıç <43832033+KantasOnur@users.noreply.github.com>
2024-09-26 18:29:09 -0700

    bitonic sort for n<=32 implemented.

--- main.cu
+++ main.cu
@@ -9,11 +9,20 @@
 
 int main()
 {
-    std::size_t const n = 4;
+    std::size_t const n = 32;
     std::size_t const switch_at = 3 * (n >> 2);
+
 
     auto data = csc485b::a1::generate_uniform< element_t >(n);
     csc485b::a1::cpu::run_cpu_baseline(data, switch_at, n);
+
+    std::cout << "unsorted: ";
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << data[i] << " ";
+    }
+    std::cout << "" << std::endl;
+
     csc485b::a1::gpu::run_gpu_soln(data, switch_at, n);
 
     return EXIT_SUCCESS;
